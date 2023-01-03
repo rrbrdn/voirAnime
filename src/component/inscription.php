58d2f71e1a458fb4d60bc3b4cb0ff583a1e5f8da
rrbrdn <brandon.f70@gmail.com>
2023-01-03 11:08:14 +0100

    update front

--- src/component/inscription.php
+++ src/component/inscription.php
@@ -84,45 +84,38 @@ if (isset($_POST['submit']) && !empty($_POST['username']) && !empty($_POST['emai
 
 <body>
 
-
-        <nav class="navbar navbar-expand-lg" id="nav">
-            <div class="navbar-center mx-auto">
-                <a class="navbar-brand" id="titre" href="./../../index.php">VoirAnime</a>
-            </div>
-        </nav>
-
-        <div class="container text-white d-flex justify-content-center mt-3">
-
-            <form action="inscription.php" method="post" enctype="multipart/form-data">
-                <fieldset class="">
-                    <div class="form-group p-1">
-                        <input type="text" class="form-control rounded-3 bg-transparent text-white border-bottom-2" name="username" placeholder="Nom d'utilisateur" autocomplete="off">
-                    </div>
-                    <hr>
-                    <div class="form-group p-1">
-                        <input type="email" class="form-control rounded-3 bg-transparent text-white border-bottom-2" id="exampleInputEmail1" aria-describedby="emailHelp"
-                            placeholder="Email" name="email" autocomplete="off">
-                    </div>
-                    <hr>
-                    <div class="form-group p-1">
-                        <input type="password" class="form-control rounded-3 bg-transparent text-white border-bottom-2" id="exampleInputPassword1" placeholder="Mot de passe"
-                            name="pdw" autocomplete="off">
-                    </div>
-                    <hr>
-                    <div class="form-group p-1">
-                        <input class="form-control rounded-3 bg-transparent text-white border-bottom-2" type="file" id="fileToUpload" name="img">
-                        <small class="form-text text-muted">Selectionnez une image de profil</small>
-                    </div>
-                    <hr>
-                    <div class="d-flex justify-content-center p-1 ">
-                        <button type="submit" id ="btn" class="btn rounded-3 w-75" name="submit">Crée un compte</button>
-                    </div>
-                </fieldset>
-            </form>
-        
+    <nav class="navbar navbar-expand-lg" id="nav">
+        <div class="navbar-center mx-auto">
+            <a class="navbar-brand" id="titre" href="./../../index.php">VoirAnime</a>
         </div>
+    </nav>
 
-
+    <div class="container text-white d-flex justify-content-center mt-5">
+        <form action="inscription.php" method="post" enctype="multipart/form-data">
+            <fieldset>
+                <div class="form-group p-1">
+                    <input type="text" class="form-control rounded-3 bg-transparent text-white border-bottom-2" name="username" placeholder="Nom d'utilisateur" autocomplete="off">
+                </div>
+                <hr>
+                <div class="form-group p-1">
+                    <input type="email" class="form-control rounded-3 bg-transparent text-white border-bottom-2" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" name="email" autocomplete="off">
+                </div>
+                <hr>
+                <div class="form-group p-1">
+                    <input type="password" class="form-control rounded-3 bg-transparent text-white border-bottom-2" id="exampleInputPassword1" placeholder="Mot de passe" name="pdw" autocomplete="off">
+                </div>
+                <hr>
+                <div class="form-group p-1">
+                    <input class="form-control rounded-3 bg-transparent text-white border-bottom-2" type="file" id="fileToUpload" name="img">
+                    <small class="form-text text-muted">Selectionnez une image de profil</small>
+                </div>
+                <hr>
+                <div class="d-flex justify-content-center p-1 ">
+                    <button type="submit" id="btn" class="btn rounded-3 w-75" name="submit">Crée un compte</button>
+                </div>
+            </fieldset>
+        </form>
+    </div>
 
 </body>
 
--- src/component/navBar.php
+++ src/component/navBar.php
@@ -1,46 +1,46 @@
 <?php
 
 session_start();
-define("URL","http://localhost/voiranime/");
+define("URL", "http://localhost/voiranime/");
 
 if (!empty($_SESSION['roleUser'])) {
     if ($_SESSION['roleUser'] == 'admin') {
 
         $nav = "<li class='nav-item'>
-                    <a class='nav-link' href='".URL."./Pages/admin/exo7.php'>admin</a>
+                    <a class='nav-link' href='" . URL . "./Pages/admin/exo7.php'>admin</a>
                 </li> 
                 <li class='nav-item'>
-                    <a class='nav-link' href='".URL."./src/component/deco.php'>deco</a>
+                    <a class='nav-link' href='" . URL . "./src/component/deco.php'>deco</a>
                 </li>";
     } elseif ($_SESSION['roleUser'] == 'user') {
         $nav = "
   
         <li class='nav-item dropdown'>
           <a class='nav-link dropdown-toggle' data-bs-toggle='dropdown' href='#' role='button' aria-haspopup='true' aria-expanded='false'> 
-            <img class='rounded-circle' width='50' src='".URL."./asset/img/".$_SESSION['img_profil']."'></a>
+            <img class='rounded-circle' width='50' src='" . URL . "./asset/img/" . $_SESSION['img_profil'] . "'></a>
           <div class='dropdown-menu'>
-            <form action='".URL."./Pages/mon-compte.php' method='post'>
-                <a class='dropdown-item'>Favoris<input hidden name='idUser' value="  . $_SESSION ['id'] .">
+            <form action='" . URL . "./Pages/mon-compte.php' method='post'>
+                <a class='dropdown-item'>Favoris<input hidden name='idUser' value="  . $_SESSION['id'] . ">
                 <button class='btn' type='submit'></button>
                 </a>
             </form>
             <div class='dropdown-divider'></div>
-            <a class='dropdown-item' href='".URL."./src/component/deco.php'>Deconnexion</a>
-            <form action='".URL."./src/component/delete-compte.php' method='post'>
-            <a class='dropdown-item'>Supprimer<input hidden name='idUser' value=" . $_SESSION ['id'] .">
+            <a class='dropdown-item' href='" . URL . "./src/component/deco.php'>Deconnexion</a>
+            <form action='" . URL . "./src/component/delete-compte.php' method='post'>
+            <a class='dropdown-item'>Supprimer<input hidden name='idUser' value=" . $_SESSION['id'] . ">
             <button class='btn' type='submit'></button>
             </a>
         </form>
           </div>
         </li>  
-                "; 
+                ";
     }
 } else {
     $nav = "    <li class='nav-item'>
                     <a class='nav-link' id='connect' href='#'>Connexion</a>
                 </li>
                 <li class='nav-item'>
-                    <a class='nav-link' id='inscription' href='".URL."./src/component/inscription.php'>Inscription</a>
+                    <a class='nav-link' id='inscription' href='" . URL . "./src/component/inscription.php'>Inscription</a>
                 </li>";
 }
 
@@ -48,7 +48,7 @@ if (!empty($_SESSION['roleUser'])) {
 
 
 
-<nav class="navbar navbar-expand-lg navbar-dark" id="nav"> 
+<nav class="navbar navbar-expand-lg navbar-dark" id="nav">
     <div class="container-fluid">
         <a class="navbar-brand" id="titre" href="./../../voiranime/index.php">VoirAnime</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
@@ -59,8 +59,8 @@ if (!empty($_SESSION['roleUser'])) {
                 <?= $nav ?>
             </ul>
             <form class="d-flex" method="POST" action="./index.php">
-                <input class="form-control me-sm-2" type="search" name="search" placeholder="Search" autocomplete="off">
-                <button class="btn btn-secondary my-2 my-sm-0" type="submit" name="envoyer">Search</button>
+                <input class="form-control bg-transparent text-white me-sm-2" type="search" name="search" placeholder="Search" autocomplete="off">
+                <button id="btn" class="btn rounded-3 my-2 my-sm-0" type="submit" name="envoyer">Search</button>
             </form>
         </div>
     </div>
